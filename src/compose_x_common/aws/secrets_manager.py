a867282481edf07ed384cd03a25eb09378f7b442
John Preston <john@ews-network.net>
2021-12-16 23:07:34 +0000

    Improved secretsmanager secret ARN RE.

--- src/compose_x_common/aws/secrets_manager.py
+++ src/compose_x_common/aws/secrets_manager.py
@@ -5,6 +5,23 @@
 import re
 
 SECRET_ARN_RE = re.compile(
-    r"^arn:aws(?:-[a-z]+)?:secretsmanager:[\w-]+:[0-9]{12}:secret:"
-    r"(?P<id>[\S]+)(?:-[A-Za-z0-9]{6,})$"
+    r"^arn:aws(?:-[a-z]+)?:secretsmanager:[\w-]+:(?P<accountid>[0-9]{12}):secret:(?P<id>[\S]+(?:-[A-Za-z0-9]{6}))$"
 )
+
+
+def get_secret_name_from_arn(secret_arn: str):
+    """
+    Function to get the secret name from the secret ARN. The ARN contains a `-` with 6 random chars.
+    The secret name needs these removed so we remove the last 7 chars from the secret ARN
+
+    :param str secret_arn:
+    :return: The secret name
+    :rtype: str
+    :raises: ValueError if invalid ARN
+    """
+    if not SECRET_ARN_RE.match(secret_arn):
+        raise ValueError(
+            "Secret ARN is not valid. Must match regex", SECRET_ARN_RE.pattern
+        )
+    secret_id = SECRET_ARN_RE.match(secret_arn).group("id")
+    return secret_id[:-7]
