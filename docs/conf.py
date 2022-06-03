af346b7a63d9c5906341c349464f6c2dc4d59231
John Preston <john@ews-network.net>
2022-06-03 09:02:23 +0100

    Docs to fallback on RTD theme if material not installed

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,13 @@
 import os
 import sys
 
-import sphinx_material
+try:
+    import sphinx_material
+
+    UNSAFE = False
+except ImportError:
+    UNSAFE = True
+
 
 sys.path.insert(0, os.path.abspath(".."))
 
@@ -19,7 +25,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "Compose-X Commons Lib"
-copyright = f"2021, @{compose_x_common.__author__}"
+copyright = f"2021-2022, @{compose_x_common.__author__}"
 author = compose_x_common.__author__
 
 version = compose_x_common.__version__
@@ -47,44 +53,39 @@ autoclass_content = "class"
 
 html_baseurl = "https://docs.common.compose-x.io"
 
-extensions.append("sphinx_material")
-html_theme_path = sphinx_material.html_theme_path()
-html_context = sphinx_material.get_html_context()
-html_theme = "sphinx_material"
-html_theme_options = {
-    # Set the name of the project to appear in the navigation.
-    "nav_title": "Compose-X Commons Lib",
-    # Set you GA account ID to enable tracking
-    # 'google_analytics_account': 'UA-XXXXX',
-    # Specify a base_url used to generate sitemap.xml. If not
-    # specified, then no sitemap will be built.
-    "base_url": "https://docs.common.compose-x.io",
-    "html_minify": False,
-    "html_prettify": True,
-    "css_minify": True,
-    # Set the color and the accent color
-    "color_primary": "blue",
-    "color_accent": "light-blue",
-    # Set the repo location to get a badge with stats
-    "repo_url": "https://github.com/compose-x/compose_x_common/",
-    "repo_name": "compose-x/compose_x_common",
-    "repo_type": "github",
-    # Visible levels of the global TOC; -1 means unlimited
-    "globaltoc_depth": 2,
-    # If False, expand all TOC entries
-    "globaltoc_collapse": True,
-    # If True, show hidden TOC entries
-    "globaltoc_includehidden": False,
-}
+if not UNSAFE:
+    extensions.append("sphinx_material")
+    html_theme_path = sphinx_material.html_theme_path()
+    html_context = sphinx_material.get_html_context()
+    html_theme = "sphinx_material"
+    html_theme_options = {
+        # Set the name of the project to appear in the navigation.
+        "nav_title": "Compose-X Commons Lib",
+        "base_url": "https://docs.common.compose-x.io",
+        "html_minify": False,
+        "html_prettify": True,
+        "css_minify": True,
+        "color_primary": "blue",
+        "color_accent": "light-blue",
+        # Set the repo location to get a badge with stats
+        "repo_url": "https://github.com/compose-x/compose_x_common/",
+        "repo_name": "compose-x/compose_x_common",
+        "repo_type": "github",
+        "globaltoc_depth": 2,
+        "globaltoc_collapse": True,
+        "globaltoc_includehidden": False,
+    }
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+    html_sidebars = {
+        "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
+    }
+
+else:
+    extensions.append("sphinx_rtd_theme")
+    html_theme = "sphinx_rtd_theme"
+
 html_static_path = ["_static"]
 html_show_sourcelink = True
-html_sidebars = {
-    "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
-}
 
 # -- Options for HTMLHelp output ---------------------------------------
 
